da51f18fbd789fd86a2299df274b6623c0ed9014
Shweta Shah <shwetushah123@gmail.com>
2020-01-03 17:04:29 +0530

    new more chnages

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+
+class abc{
+    
+    var a : String?
+    
+    init?(a: String){
+    
+        if a != nil {
+            self.a = a
+        }else{
+            return nil
+        }
+        
+    }
+    
+    
+    
+    
+}
+
